47f20b2a9d72ec1f0b3f9af76bf4463ad2d0d82a
Larry Ling <larryling.main@gmail.com>
2026-02-01 02:32:21 -0800

    feat: add mask button layout + add multiple levels

--- Assets/Scripts/MaskInfoPanel.cs
+++ Assets/Scripts/MaskInfoPanel.cs
@@ -61,6 +61,12 @@ public class MaskInfoPanel : MonoBehaviour
         textBox.SetActive(false);
         resultBoxObject.SetActive(true);
 
+        Mask[] allMasks = FindObjectsByType<Mask>(FindObjectsSortMode.None);
+        foreach (Mask mask in allMasks)
+        {
+            mask.gameObject.SetActive(false);
+        }
+
         if (selectedMask == correctMask)
         {
             resultBox.ShowResult("Wonderful! Thank you, I can already feel this is exactly what I need. I wish you the best.");
